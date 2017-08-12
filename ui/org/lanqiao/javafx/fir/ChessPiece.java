7744cd85294b0eae986697811f0977900b33d62e
zhengwei <zhengwei223@163.com>
2017-08-12 16:50:59 +0800

    爬虫ajax-json和cookie

---
+++ ui/org/lanqiao/javafx/fir/ChessPiece.java
@@ -0,0 +1,21 @@
+package org.lanqiao.javafx.fir;
+
+/**
+ * 棋子
+ *
+ * @author Mi Zhou
+ */
+public enum ChessPiece {
+
+	EMPTY(0), WHITE(1), BLACK(2);
+
+	private final int val;
+
+	private ChessPiece(int val) {
+		this.val = val;
+	}
+
+	public int val() {
+		return val;
+	}
+}
